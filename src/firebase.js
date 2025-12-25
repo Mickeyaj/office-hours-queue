b2963fa168b07fb08230a3e79d894bc0fd0b6568
Mickeyaj <mikaylaja28@gmail.com>
2025-12-25 09:35:40 -0800

    Use environment variables for Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDT-JxNhj8GuHWnt9IAKXjPVfWpKfJ6auo",
-  authDomain: "office-hour-queue-ccf68.firebaseapp.com",
-  projectId: "office-hour-queue-ccf68",
-  storageBucket: "office-hour-queue-ccf68.firebasestorage.app",
-  messagingSenderId: "207671968614",
-  appId: "1:207671968614:web:e6dc15cc9ec117e0897ea7",
-  measurementId: "G-7MP1ZVFFCG"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASEE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
